53f67e64015a6dfb6185da3140efda8b7567aa45
Shlomo Feit <11.5sfeit@gmail.com>
2026-06-17 12:37:50 +0300

    MySQL connection completion in the initial version.

---
+++ database/db_connection.py
@@ -0,0 +1,69 @@
+from mysql import connector
+
+
+class DBConnection:
+
+    conn = None
+
+    @classmethod
+    def connect(cls):
+
+        try:
+            cls.conn = connector.connect(
+                host = "localhost",
+                user = "root",
+                password = "1234",
+                port = 3306,
+                database = "Intelligence_db"
+            )
+        except connector.errors.ProgrammingError:
+            with cls.conn.cursor() as cursor:
+                cursor.execute("CREATE DATABASE IF NOT EXISTS Intelligence_db; USE Intelligence_db")
+                cls.connect()
+
+        return cls.conn
+    
+    @classmethod
+    def get_connection(cls):
+        if cls.conn and cls.conn.is_connected():
+            return cls.conn
+        
+        cls.connect()
+        cls.create_tabels()
+        return cls.conn
+    
+
+    @classmethod
+    def create_tabels(cls):
+
+        cursor = cls.conn.cursor()
+        cursor.execute("""
+                       CREATE TABLE IF NOT EXISTS agents(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        specialty VARCHAR(50) NOT NULL,
+        is_active BOOLEAN DEFAULT TRUE NOT NULL,
+        completed_missions INT DEFAULT 0 NOT NULL,
+        failed_missions INT DEFAULT 0 NOT NULL,
+        agent_runk ENUM('Junior', 'Senior', 'Commander') NOT NULL
+        );
+        CREATE TABLE missions(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(100) NOT NULL,
+        description TEXT NOT NULL,
+        location VARCHAR(100) NOT NULL,
+        difficulty INT NOT NULL,
+        importance INT NOT NULL,
+        status ENUM('NEW', 'ASSIGNED', 'IN_PROGRESS', 'COMPLETED', 'FAILED', 'CANCELLED'),
+        risk_level ENUM('LOW', 'MEDIUM', 'HIGH', 'CRITICAL'),
+        assigned_agent_id INT
+        );
+                       """)
+
+        cursor.close()
+
+
+    @classmethod
+    def close(cls):
+        if cls.conn and cls.conn.is_connected():
+            cls.conn.close()
